672458f7441c4ae5a95d296d81ffd4b0764366ab
Aspen James <aspenjames@tqca.org>
2018-03-06 21:27:05 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,11 @@
 class Book
-  attr_accessor :title :author :genre
+  attr_accessor :title, :author, :genre
 
   def initialize(title)
     @title = title
   end
 
   def turn_page
-    
+
   end
 end
